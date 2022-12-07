ec43f7492e24285bb7f309a5c5b11f17cdf488eb
Daniele Arminio <87833584+d3xplo1t@users.noreply.github.com>
2022-12-07 16:51:23 +0000

    Create collagemaker.py

---
+++ collagemaker.py
@@ -0,0 +1,29 @@
+# Import the Python Imaging Library
+from PIL import Image
+
+# Load the image
+image = Image.open("image.jpg")
+
+# Create a new image that is the size of an A4 sheet
+a4_sheet = Image.new('RGBA', (2480, 3508), color=(0, 0, 0, 0))
+
+# Set the number of rows and columns on the A4 sheet
+rows = 8
+cols = 5
+
+# Calculate the width and height of each image
+image_width = a4_sheet.width // cols
+image_height = a4_sheet.height // rows
+
+# Iterate over the rows and columns
+for i in range(rows):
+    for j in range(cols):
+        # Calculate the coordinates for the current image
+        x = j * image_width
+        y = i * image_height
+
+        # Paste the image at the current position
+        a4_sheet.paste(image, (x, y))
+
+# Save the resulting image to a file
+a4_sheet.save("a4_sheet.png")
